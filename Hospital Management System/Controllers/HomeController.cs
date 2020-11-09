31c8d31f0bf024c83f3275137c96830ca2faf133
u16220073 <u16220073@tuks.co.za>
2020-11-09 05:05:13 +0200

    added help links on relevent pages

--- Hospital Management System/Controllers/HomeController.cs
+++ Hospital Management System/Controllers/HomeController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
 using System.Web.Mvc;
-using Hospital_Management_System.Models;
+using GroupDocs.Viewer;
+using GroupDocs.Viewer.Options;
 
 namespace Hospital_Management_System.Controllers
 {
@@ -18,5 +17,30 @@ namespace Hospital_Management_System.Controllers
         {
             return View();
         }
+
+        public ActionResult ViewHelpDoc()
+        {
+            try
+            {
+                var path = Server.MapPath("~/Content/help/HELP_DOCUMENT.pdf");
+                var AccesFilePath = Server.MapPath("~/Content/uploads/HELP_DOCUMENT.pdf");
+                using (Viewer viewerObject = new Viewer(path))
+                {
+                    PdfViewOptions options = new PdfViewOptions(AccesFilePath);
+                    viewerObject.View(options);
+                }
+
+                var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                var fsResult = new FileStreamResult(fileStream, "application/pdf");
+                return fsResult;
+            }catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+            //if we got here something went wrong
+            return View();
+        }
+
     }
 }
